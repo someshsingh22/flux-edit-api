0e482fb576a90496eda3855f9953b2031ad1e3c0
someshs <someshs@adobe.com>
2024-11-29 16:27:41 +0000

    Remove Port Change

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ import uuid
 
 app = FastAPI()
 
-xflux_pipeline = XFluxPipeline("flux-dev", os.environ.get("FLUX_DEVICE", "cuda:6"), False)
+xflux_pipeline = XFluxPipeline(
+    "flux-dev", os.environ.get("FLUX_DEVICE", "cuda:6"), False
+)
 xflux_pipeline.set_ip(None, "XLabs-AI/flux-ip-adapter-v2", "ip_adapter.safetensors")
+
 
 class GenerateRequest(BaseModel):
     prompt: str
@@ -22,9 +25,12 @@ class GenerateRequest(BaseModel):
     width: int = 1024
     height: int = 1024
 
+
 @app.post("/generate/")
 async def generate(request: GenerateRequest):
-    image_prompt = Image.open(request.img_prompt_path) if request.img_prompt_path else None
+    image_prompt = (
+        Image.open(request.img_prompt_path) if request.img_prompt_path else None
+    )
 
     result = xflux_pipeline(
         prompt=request.prompt,
@@ -38,13 +44,9 @@ async def generate(request: GenerateRequest):
     )
 
     img_path, ext = request.img_prompt_path.split(".")
-    save_path = img_path + "_variantx_" + str(uuid.uuid4()) + '.' + ext
+    save_path = img_path + "_variantx_" + str(uuid.uuid4()) + "." + ext
     if not os.path.exists(save_path):
         os.mkdir(save_path)
     result.save(save_path)
 
     return {"output_path": save_path}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=os.environ.get("PORT", 8008))
