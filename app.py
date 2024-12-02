ffdc2d42f27e8ff762b5cee46aec1dfd9958136e
someshs <someshs@adobe.com>
2024-12-02 04:01:08 +0000

    Small Filename BugFix

--- app.py
+++ app.py
@@ -43,10 +43,8 @@ async def generate(request: GenerateRequest):
         image_prompt=image_prompt,
     )
 
-    img_path, ext = request.img_prompt_path.split(".")
-    save_path = img_path + "_variantx_" + str(uuid.uuid4()) + "." + ext
-    if not os.path.exists(save_path):
-        os.mkdir(save_path)
+    i = request.img_prompt_path.rfind('.')
+    save_path = request.img_prompt_path[:i] +  "_variantx_" + request.img_prompt_path[i:]
     result.save(save_path)
 
     return {"output_path": save_path}
